5b17ab8e8731ad0293bca87f76265274c7753852
Sledmine <40512223+Sledmine@users.noreply.github.com>
2025-01-18 05:45:28 -0600

    add hsc runtime bridge module

---
+++ lua/modules/hsc.lua
@@ -0,0 +1,84 @@
+local hsc = {}
+
+local executeScript = Engine.hsc.executeScript
+
+local luna = require "luna"
+
+local hscDoc = require "hscDoc"
+
+local cacheHscGlobals = {
+    boolean = "lua_boolean",
+    short = "lua_short",
+    long = "lua_long",
+    real = "lua_real",
+    string = "lua_string",
+    unit = "lua_unit",
+    object = "lua_object",
+    object_list = "lua_object_list"
+}
+
+local function getScriptArgs(args)
+    return table.map(args, tostring)
+end
+
+local function getFunctionInvokation(hscFunction, args)
+    return hscFunction.funcName .. " " .. table.concat(args, " ")
+end
+
+local function setVariable(varName, varValue)
+    local varSet = "begin (set " .. varName .. " (" .. varValue .. "))"
+    executeScript(varSet)
+end
+
+local function getVariable(varName)
+    return get_global(varName)
+end
+
+setmetatable(hsc, {
+    __index = function(_, key)
+        local hscFunction = table.find(hscDoc, function(doc)
+            return doc.funcName:trim() == key
+        end)
+        if hscFunction then
+            local returnType = hscFunction.returnType
+            -- Handle compatible primitive types trough Lua
+            if returnType == "boolean" or returnType == "short" or returnType == "long" or
+                returnType == "real" then
+                return function(...)
+                    local args = getScriptArgs({...})
+                    local functionInvokation = getFunctionInvokation(hscFunction, args)
+                    setVariable(cacheHscGlobals[returnType], functionInvokation)
+                    local result = getVariable(cacheHscGlobals[returnType])
+                    if returnType == "boolean" then
+                        if key == "objects_can_see_object" then
+                            logger:debug("{} -> {}", functionInvokation, result)
+                        end
+                        result = luna.bool(result)
+                    end
+                    return result
+                end
+            -- Let game hs script execution handle not primitive types
+            elseif returnType == "object" or returnType == "unit" or returnType == "object_list" then
+                return function(...)
+                    local args = table.map({...}, tostring)
+                    local functionInvokation = getFunctionInvokation(hscFunction, args)
+                    return "(" .. functionInvokation .. ")"
+                end
+            elseif returnType ~= "void" then
+                logger:warning("Return type {} for {} not implemented", returnType, key)
+            else
+                return function(...)
+                    local args = table.map({...}, tostring)
+                    logger:debug("Invoking function {}", key)
+                    local functionInvokation = getFunctionInvokation(hscFunction, args)
+                    logger:debug(functionInvokation)
+                    executeScript(functionInvokation)
+                end
+            end
+        else
+            error("Function " .. key .. " not found in HSC documentation")
+        end
+    end
+})
+
+return hsc
